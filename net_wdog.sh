f0a0ffff267817623e0bb74ae198aa1992c72003
Ricardo <ricardovelero@gmail.com>
2018-07-12 11:34:21 +0200

    Simplify detection of net problem

--- net_wdog.sh
+++ net_wdog.sh
@@ -4,32 +4,17 @@
 
 
 # add ip / hostname separated by white space
-HOSTS="ethermine.org theos.in solucionesio.es"
+HOST="ethermine.org"
 
 # email report when
 SUBJECT="Ping failed"
 EMAILID="prospector@localhost"
 
-# no ping request
 COUNT=4
-RECOUNT=0
-re='^[0-9]+$'
 
-for myHost in $HOSTS
-do
-	count=$(ping -c $COUNT $myHost | grep 'received' | awk -F',' '{ print $2 }' | awk '{ print $1 }')
-	if [[ $COUNT -lt 2 ]] || [[ $COUNT = *"unknown host"* ]]; then
-		echo "Host : $myHost is down (ping failed) at $(date)" | mail -s "$SUBJECT" $EMAILID
-	fi
-	if [[ $COUNT =~ $re ]] ; then
-		RECOUNT=$(($RECOUNT+$count))
-	else
-		echo "Host : could not resolve $myHost (ping failed) at $(date)" | mail -s "$SUBJECT" $EMAILID
-		sudo systemctl restart networking.service >/dev/null 2>&1
-	fi
-done
+PING=$(ping -c $COUNT $HOST | grep 'received' | awk -F',' '{ print $2 }' | awk '{ print $1 }')
 
-if [[ $RECCOUNT -lt 8 ]]; then
-	# 70% failed, restart network service
-	sudo systemctl restart networking.service >/dev/null 2>&1
+if [[ $PING -lt 2 ]] || [[ $PING = *"unknown host"* ]]; then
+	echo "$HOST is down (ping failed) at $(date). Will try to restart networking" | mail -s "$SUBJECT" $EMAILID
+	sudo systemctl restart networking.service
 fi
