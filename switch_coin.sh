12b424aa27520df5e2b0719d057e645a99ddafbd
Ricardo <ricardovelero@gmail.com>
2018-10-02 12:03:55 +0200

    Test the input for ETH, ETC, BTG

--- switch_coin.sh
+++ switch_coin.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-if [[ ! -z "$1" ]]; then
+if [[ ! -z "$1" ]] || [[ $1 = *[ETHETCBTG]* ]]; then
   sed -i.bak '/COIN/d' ~/settings.conf
   echo >> ~/settings.conf
   echo 'COIN="$1"' >> ~/settings.conf
   sudo restart
 else
-  CURRENT_COIN=$(cat ~/settings.conf | grep "COIN")
-  echo "You need to specify the coin type (ETH, ETC, BTG) to switch to. The current is: $CURRENT_COIN"
+  echo "You need to specify the correct coin type (ETH, ETC, BTG) to switch to. The current is: $(cat ~/settings.conf | grep 'COIN')"
 fi
